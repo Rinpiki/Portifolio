48987b5a8eb7431bb062b9ee16d9cee582247d14
Marcos Pablo <rinpikibr@gmail.com>
2022-10-19 22:30:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
